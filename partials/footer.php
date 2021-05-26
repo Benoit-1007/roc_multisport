0af17d850dcbc2e4b26dc7e766d02e92bc6ac109
Benoit-1007 <benoit.flipot1007@gmail.com>
2021-05-26 23:35:19 +0200

    Update home page

--- partials/footer.php
+++ partials/footer.php
@@ -1,12 +1,12 @@
 <img class="logo" src="resources/logoRocMultisport.png" alt="Logo ROC Multisport">
-        <address>
-            <p>165 route des Tappes</p>
-            <p>74230 Dingy-Saint-Clair</p>
-            <p><a href="tel:0658355166">Tél. : 06 58 35 51 66</a></p>
-            <p><a href="mailto:roc.multisport@gmail.com" title="Cliquez içi pour nous contacter">roc.multisport@gmail.com</a></p>
-        </address>
-        <div class="network">
-            <p>Retrouver-nous sur :</p>
-            <a href="#" class="fab fa-facebook-square" title="Envie de découvrir notre page facebook? Cliquez içi !"></a>
-            <a href="#" class="fab fa-instagram-square" title="Pour découvrir d'autres photos de nos acivités, cliquez içi !"></a>
-        </div>
+<address>
+    <p>165 route des Tappes</p>
+    <p>74230 Dingy-Saint-Clair</p>
+    <p><a href="tel:0658355166">Tél. : 06 58 35 51 66</a></p>
+    <p><a href="mailto:roc.multisport@gmail.com" title="Cliquez içi pour nous contacter">roc.multisport@gmail.com</a></p>
+</address>
+<div class="network">
+    <p>Retrouver-nous sur :</p>
+    <a href="#" class="fab fa-facebook-square" title="Envie de découvrir notre page facebook? Cliquez içi !"></a>
+    <a href="#" class="fab fa-instagram-square" title="Pour découvrir d'autres photos de nos acivités, cliquez içi !"></a>
+</div>
