4d334c6ef8f56cdcc676c35f284bdff3f1677b5e
Arnaud Papa <arnaud.papa@infogene.fr>
2021-06-17 12:38:21 +0200

    Update backBooking.js

--- js/backBooking.js
+++ js/backBooking.js
@@ -105,110 +105,116 @@ function showDetails(identifier) {
             </table>`;
 
             whereToWrite.innerHTML = read_one_product_html;
-        });
 
-    // read contact record based on given contact ID
-    fetch('api/contact/readOneContactDetails.php?idContact=' + id)
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data)
-            var read_one_contact_html = `
-                <h3>Contact</h3>
-                <!-- contact data will be shown in this table -->
-                <table>
-                
-                    <!-- Contact Id -->
-                    <tr>
-                        <td>ID</td>
-                        <td>` + data.idContact + `</td>
-                    </tr>
-                
-                    <!-- Contact details -->
-                    <tr>
-                        <td>Nom</td>
-                        <td>` + data.lastName + `</td>
-                    </tr>
-
-                    <tr>
-                        <td>Prénom</td>
-                        <td>` + data.firstName + `</td>
-                    </tr>
-                
-                    <tr>
-                        <td>Société</td>
-                        <td>` + data.organisation + `</td>
-                    </tr>
-
-                    <tr>
-                        <td>Téléphone</td>
-                        <td>` + data.phoneNumber + `</td>
-                    </tr>
-
-                    <tr>
-                        <td>Mail</td>
-                        <td>` + data.mail + `</td>
-                    </tr>
-
-                    <tr>
-                        <td>Adresse</td>
-                        <td>` + data.adress + `</td>
-                    </tr>
-
-                    <tr>
-                        <td>Code Postal</td>
-                        <td>` + data.postalCode + `</td>
-                    </tr>
-
-                    <tr>
-                        <td>Ville</td>
-                        <td>` + data.city + `</td>
-                    </tr>  
-                
-                </table>`;
-
-            whereToWrite.innerHTML += read_one_contact_html;
-        });
-
-    // read activities record based on given booking ID
-    fetch('api/activity/readActivitiesList.php?idBooking=' + id)
-        .then(res => res.json())
-        .then((data) => {
-
-            var read_activities_html = `
-
-            <h3>Activities</h3>
-            <!-- start table -->
-            <table>
-            
-                <tr>
-                    <th>Code</th>
-                    <th>Nom</th>
-                    <th>Date</th>
-                    <th>Journée</th>
-                </tr>`;
-
-            // loop through returned list of data
-            (data.records.forEach((key, val) => {
-
-                console.log(key)
-                // creating new table row per record
-                read_activities_html += `
-                <tr>
+            // read contact record based on given contact ID
+            fetch('api/contact/readOneContactDetails.php?idContact=' + id)
+                .then(res => res.json())
+                .then((data) => {
+                    console.log(data)
+                    var read_one_contact_html = `
+        <h3>Contact</h3>
+        <!-- contact data will be shown in this table -->
+        <table>
         
-                    <td>` + key.codeActivity + `</td>
-                    <td>` + key.nameActivity + `</td>
-                    <td>` + key.dateActivity + `</td>
-                    <td>` + key.halfDaySelect + `</td>       
-
+            <!-- Contact Id -->
+            <tr>
+                <td>ID</td>
+                <td>` + data.idContact + `</td>
+            </tr>
         
-                </tr>`;
-            }));
+            <!-- Contact details -->
+            <tr>
+                <td>Nom</td>
+                <td>` + data.lastName + `</td>
+            </tr>
 
-            // end table
-            read_activities_html += `</table>`;
+            <tr>
+                <td>Prénom</td>
+                <td>` + data.firstName + `</td>
+            </tr>
+        
+            <tr>
+                <td>Société</td>
+                <td>` + data.organisation + `</td>
+            </tr>
 
-            whereToWrite.innerHTML += read_activities_html;
+            <tr>
+                <td>Téléphone</td>
+                <td>` + data.phoneNumber + `</td>
+            </tr>
+
+            <tr>
+                <td>Mail</td>
+                <td>` + data.mail + `</td>
+            </tr>
+
+            <tr>
+                <td>Adresse</td>
+                <td>` + data.adress + `</td>
+            </tr>
+
+            <tr>
+                <td>Code Postal</td>
+                <td>` + data.postalCode + `</td>
+            </tr>
+
+            <tr>
+                <td>Ville</td>
+                <td>` + data.city + `</td>
+            </tr>  
+        
+        </table>`;
+
+                    whereToWrite.innerHTML += read_one_contact_html;
+
+                    // read activities record based on given booking ID
+                    fetch('api/activity/readActivitiesList.php?idBooking=' + id)
+                        .then(res => res.json())
+                        .then((data) => {
+
+                            var read_activities_html = `
+
+    <h3>Activities</h3>
+    <!-- start table -->
+    <table>
+    
+        <tr>
+            <th>Code</th>
+            <th>Nom</th>
+            <th>Date</th>
+            <th>Journée</th>
+        </tr>`;
+
+                            // loop through returned list of data
+                            (data.records.forEach((key, val) => {
+
+                                console.log(key)
+                                // creating new table row per record
+                                read_activities_html += `
+        <tr>
+
+            <td>` + key.codeActivity + `</td>
+            <td>` + key.nameActivity + `</td>
+            <td>` + key.dateActivity + `</td>
+            <td>` + key.halfDaySelect + `</td>       
+
+
+        </tr>`;
+                            }));
+
+                            // end table
+                            read_activities_html += `</table>`;
+
+                            whereToWrite.innerHTML += read_activities_html;
+                        });
+
+                });
+
         });
+
+
+
+
 }
 
 
